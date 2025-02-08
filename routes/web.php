0427506eb2f9ba870707d4fccdc044d2da4f16f3
NMarcell0001 <neme0001@hz.nl>
2025-02-08 14:52:24 +0100

    IndexController added, working!

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
-
+use App\Http\Controllers\IndexController;
+Route::get('/', [IndexController::class, 'index']);
