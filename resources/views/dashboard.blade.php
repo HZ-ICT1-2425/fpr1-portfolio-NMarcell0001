e565d493dff96aca37ea3692dfe48a204649a586
NMarcell0001 <neme0001@hz.nl>
2025-02-09 09:36:54 +0100

    Dashboard updated along with CSS, some stuff was missing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,37 +19,37 @@
                 <td>Course timing</td>
                 <td>Course status</td>
                 <td>Course grade received</td>
-                <td>Overall EC / Grade</td>
+                <td>Overall EC</td>
             </tr>
-            <tr class="underwaydashentries">
+            <tr class="completeddashentries">
                 <td>CU75001</td>
                 <td>Program- & Career Orientation</td>
                 <td>2.5 EC</td>
                 <td>Portfolio website (Presentation)</td>
                 <td>Block 1</td>
-                <td>Underway</td>
-                <td>--</td> <!-- PCO GRADE-->
-                <td>-/-</td> <!-- OVERALL EC / GRADE-->
+                <td>Completed</td>
+                <td>9.3</td> <!-- PCO GRADE-->
+                <td>2.5</td> <!-- OVERALL EC-->
             </tr>
-            <tr class="underwaydashentries">
+            <tr class="completeddashentries">
                 <td>CU75002</td>
                 <td>Computer Science Basics</td>
                 <td>5 EC</td>
                 <td>Written knowledge test</td>
                 <td>Block 1</td>
-                <td>Underway</td>
-                <td>--</td> <!-- COMPSCI GRADE-->
-                <td>-/-</td> <!-- OVERALL EC / GRADE-->
+                <td>Completed</td>
+                <td>9.1</td> <!-- COMPSCI GRADE-->
+                <td>7.5</td> <!-- OVERALL EC-->
             </tr>
-            <tr class="underwaydashentries">
+            <tr class="completeddashentries">
                 <td>CU75003</td>
                 <td>Programming Basics</td>
                 <td>5 EC</td>
                 <td>Written knowledge test</td>
                 <td>Block 1</td>
-                <td>Underway</td>
-                <td>--</td> <!-- PROGRAMMING BASICS GRADE-->
-                <td>-/-</td> <!-- OVERALL EC / GRADE-->
+                <td>Completed</td>
+                <td>6.5</td> <!-- PROGRAMMING BASICS GRADE-->
+                <td>12.5</td> <!-- OVERALL EC-->
             </tr>
             <tr class="underwaydashentries">
                 <td>CU75068</td>
@@ -59,54 +59,54 @@
                 <td>Block 1-4</td>
                 <td>Underway</td>
                 <td>--</td> <!-- PPDE GRADE-->
-                <td>-/-</td> <!-- OVERALL EC / GRADE-->
+                <td>-/-</td> <!-- OVERALL EC-->
             </tr>
-            <tr class="dashentries">
+            <tr class="underwaydashentries">
                 <td>-----</td>
                 <td>IT Personality</td>
                 <td>2.5 EC</td>
                 <td>Portfolio</td>
                 <td>Block 2-4</td>
-                <td>Not started</td>
-                <td>--</td> <!-- IT PERSONALITY GRADE-->
-                <td>-/-</td> <!-- OVERALL EC / GRADE-->
+                <td>Underway</td>
+                <td>1/2 PASS</td> <!-- IT PERSONALITY GRADE-->
+                <td>13.75 EC</td> <!-- OVERALL EC-->
             </tr>
-            <tr class="dashentries">
+            <tr class="completeddashentries">
                 <td rowspan="2">CU75004</td>
                 <td rowspan="2">Object Oriented Programming</td>
                 <td>5 EC</td>
                 <td>Presentation</td>
                 <td>Block 2</td>
-                <td>Not started</td>
-                <td>--</td> <!-- OOP PRESENTATION PROG GRADE-->
-                <td rowspan="2">-/-</td> <!-- OVERALL EC / GRADE-->
+                <td>Completed</td>
+                <td>8.8</td> <!-- OOP PRESENTATION PROG GRADE-->
+                <td rowspan="2">23.75 EC</td> <!-- OVERALL EC-->
             </tr>
-            <tr class="dashentries">
+            <tr class="completeddashentries">
                 <td>5 EC</td>
                 <td>Written knowledge test</td>
                 <td>Block 2</td>
-                <td>Not started</td>
-                <td>--</td> <!-- OOP WRITTEN TEST GRADE-->
+                <td>Completed</td>
+                <td>8.6</td> <!-- OOP WRITTEN TEST GRADE-->
             </tr>
-            <tr class="dashentries">
+            <tr class="underwaydashentries">
                 <td>CU75081</td>
                 <td>Business IT Consultancy Basics</td>
                 <td>2.5 EC</td>
                 <td>Assignment</td>
                 <td>Block 3-4</td>
-                <td>Not started</td>
+                <td>Underway</td>
                 <td>--</td> <!-- BUSINESS IT GRADE-->
-                <td>-/-</td> <!-- OVERALL EC / GRADE-->
+                <td>-/-</td> <!-- OVERALL EC-->
             </tr>
-            <tr class="dashentries">
+            <tr class="underwaydashentries">
                 <td rowspan="4">CU75080</td>
                 <td rowspan="4">Framework Project 1</td>
                 <td>5 EC</td>
                 <td>Written knowledge test</td>
                 <td>Block 3</td>
-                <td>Not started</td>
+                <td>Underway</td>
                 <td>--</td> <!-- FP1 GRADE-->
-                <td rowspan="4">-/-</td> <!-- OVERALL EC / GRADE-->
+                <td rowspan="4">-/-</td> <!-- OVERALL EC-->
             </tr>
             <tr class="dashentries">
                 <td>1.25 EC</td>
@@ -137,7 +137,7 @@
                 <td>Block 4</td>
                 <td>Not started</td>
                 <td>--</td> <!--FP2 GRADE-->
-                <td rowspan="3">-/-</td> <!-- OVERALL EC / GRADE-->
+                <td rowspan="3">-/-</td> <!-- OVERALL EC-->
             </tr>
             <tr class="dashentries">
                 <td>2.5 EC</td>
@@ -156,7 +156,7 @@
             <tr class="credits">
                 <td>-----</td>
                 <td>Current EC amount:</td>
-                <td>0 EC</td>
+                <td>23.75 EC</td>
                 <td>-----</td>
                 <td>----</td>
                 <td>Overall result:</td>
